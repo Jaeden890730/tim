02ab2a79bfe56d3b824f8d80a96377926fff880b
Jaeden890730 <141991510+Jaeden890730@users.noreply.github.com>
2026-05-13 06:36:51 +0800

    Create TFBomRouteBean.java

---
+++ src/com/mxic/oiplus/oisearch/TFBomRouteBean.java
@@ -0,0 +1,379 @@
+package com.mxic.oiplus.oisearch;
+
+/**
+ * <p>Title: </p>
+ *
+ * <p>Description: </p>
+ *
+ * <p>Copyright: Copyright (c) 2006</p>
+ *
+ * <p>Company: </p>
+ *
+ * @author not attributable
+ * @version 1.0
+ */
+public class TFBomRouteBean {
+  public TFBomRouteBean() {
+  }
+
+  private String sid;
+  private String product_body;
+  private String brand;
+  private String version;
+  private String backend_option;
+  private String fg_with_code;
+  private String pin_count;
+  private String package_type;
+  private String grade;
+  private String ft_route_code;
+  private String ft_route;
+  private String mask_option;
+  private String sort_route_code;
+  private String db_with_code;
+  private String ws_route;
+  private String ws_route_add;
+  private String tf_comment;
+  private String tf_ws_comment;
+  private String ftAddroute;
+  private String ftAddroute2;
+  private String ftAddroute3;
+  private String sales_form;
+  private String endurance;
+  private String avi;
+  private String ink;
+  private String tag;
+  private String group_key;
+  private String wsspecialcontrol;
+  private String quality_level;
+  private String quality_level_comment;
+  
+  private String mcp_flag;
+  
+  private String component_no;
+  private String com_prod_body;
+  private String com_mask_option;
+  private String com_backend_option;
+  
+  public void setSid(String sid) {
+    this.sid = sid;
+  }
+
+  public String getSid() {
+    return sid;
+  }
+
+  public void setProduct_body(String product_body) {
+    this.product_body = product_body;
+  }
+
+  public String getProduct_body() {
+    return product_body;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setBackend_option(String backend_option) {
+    this.backend_option = backend_option;
+  }
+
+  public String getBackend_option() {
+    return backend_option;
+  }
+
+  public void setFg_with_code(String fg_with_code) {
+    this.fg_with_code = fg_with_code;
+  }
+
+  public String getFg_with_code() {
+    return fg_with_code;
+  }
+
+  public void setPin_count(String pin_count) {
+    this.pin_count = pin_count;
+  }
+
+  public String getPin_count() {
+    return pin_count;
+  }
+
+  public void setPackage_type(String package_type) {
+    this.package_type = package_type;
+  }
+
+  public String getPackage_type() {
+    return package_type;
+  }
+
+  public void setGrade(String grade) {
+    this.grade = grade;
+  }
+
+  public String getGrade() {
+    return grade;
+  }
+
+  public void setFt_route_code(String ft_route_code) {
+    this.ft_route_code = ft_route_code;
+  }
+
+  public String getFt_route_code() {
+    return ft_route_code;
+  }
+
+  public void setFt_route(String ft_route) {
+    this.ft_route = ft_route;
+  }
+
+  public String getFt_route() {
+    return ft_route;
+  }
+
+  public void setMask_option(String mask_option) {
+    this.mask_option = mask_option;
+  }
+
+  public String getMask_option() {
+    return mask_option;
+  }
+
+  public void setSort_route_code(String sort_route_code) {
+    this.sort_route_code = sort_route_code;
+  }
+
+  public String getSort_route_code() {
+    return sort_route_code;
+  }
+
+  public void setDb_with_code(String db_with_code) {
+    this.db_with_code = db_with_code;
+  }
+
+  public String getDb_with_code() {
+    return db_with_code;
+  }
+
+  public void setWs_route(String ws_route) {
+    this.ws_route = ws_route;
+  }
+
+  public String getWs_route() {
+    return ws_route;
+  }
+
+  public void setWs_route_add(String ws_route_add) {
+    this.ws_route_add = ws_route_add;
+  }
+
+  public String getWs_route_add() {
+    return ws_route_add;
+  }
+
+  public void setTf_comment(String tf_comment) {
+    this.tf_comment = tf_comment;
+  }
+
+  public String getTf_comment() {
+    return tf_comment;
+  }
+
+  public void setTf_ws_comment(String tf_ws_comment) {
+    this.tf_ws_comment = tf_ws_comment;
+  }
+
+  public String getTf_ws_comment() {
+    return tf_ws_comment;
+  }
+
+  public String getFtAddroute() {
+    return ftAddroute;
+  }
+
+  public void setFtAddroute(String ftAddroute) {
+    this.ftAddroute = ftAddroute;
+  }
+
+  /**
+   * @return the sales_form
+   */
+  public String getSales_form() {
+	  return sales_form;
+  }
+
+  /**
+   * @param sales_form the sales_form to set
+   */
+  public void setSales_form(String sales_form) {
+	  this.sales_form = sales_form;
+  }
+
+  /**
+   * @return the tag
+   */
+  public String getTag() {
+	  return tag;
+  }
+
+  /**
+   * @param tag the tag to set
+   */
+  public void setTag(String tag) {
+	  this.tag = tag;
+  }
+
+  /**
+   * @return the endurace
+   */
+  public String getEndurance() {
+	  return endurance;
+  }
+
+  /**
+   * @param endurance the endurance to set
+   */
+  public void setEndurance(String endurance) {
+	  this.endurance = endurance;
+  }
+
+  /**
+   * @return the avi
+   */
+  public String getAvi() {
+	  return avi;
+  }
+
+  /**
+   * @param avi the avi to set
+   */
+  public void setAvi(String avi) {
+	  this.avi = avi;
+  }
+
+  /**
+   * @return the ink
+   */
+  public String getInk() {
+	  return ink;
+  }
+
+  /**
+   * @param ink the ink to set
+   */
+  public void setInk(String ink) {
+	  this.ink = ink;
+  }
+  
+  /**
+   * @return the group_key
+   */
+  public String getGroup_key() {
+	  return group_key;
+  }
+
+  /**
+   * @param group_key the group_key to set
+   */
+  public void setGroup_key(String group_key) {
+	  this.group_key = group_key;
+  }
+
+/**
+ * @return the wsspecialcontrol
+ */
+public String getWsspecialcontrol() {
+	return wsspecialcontrol;
+}
+
+/**
+ * @param wsspecialcontrol the wsspecialcontrol to set
+ */
+public void setWsspecialcontrol(String wsspecialcontrol) {
+	this.wsspecialcontrol = wsspecialcontrol;
+}
+
+public String getFtAddroute2() {
+    return ftAddroute2;
+}
+
+public void setFtAddroute2(String ftAddroute2) {
+    this.ftAddroute2 = ftAddroute2;
+}
+
+public String getFtAddroute3() {
+    return ftAddroute3;
+}
+
+public void setFtAddroute3(String ftAddroute3) {
+    this.ftAddroute3 = ftAddroute3;
+}
+
+public String getQuality_level() {
+    return quality_level;
+}
+
+public void setQuality_level(String quality_level) {
+    this.quality_level = quality_level;
+}
+
+public String getQuality_level_comment() {
+    return quality_level_comment;
+}
+
+public void setQuality_level_comment(String quality_level_comment) {
+    this.quality_level_comment = quality_level_comment;
+}
+
+public String getMcp_flag() {
+	return mcp_flag;
+}
+
+public void setMcp_flag(String mcp_flag) {
+	this.mcp_flag = mcp_flag;
+}
+
+public String getComponent_no() {
+	return component_no;
+}
+
+public void setComponent_no(String component_no) {
+	this.component_no = component_no;
+}
+
+public String getCom_prod_body() {
+	return com_prod_body;
+}
+
+public void setCom_prod_body(String com_prod_body) {
+	this.com_prod_body = com_prod_body;
+}
+
+public String getCom_mask_option() {
+	return com_mask_option;
+}
+
+public void setCom_mask_option(String com_mask_option) {
+	this.com_mask_option = com_mask_option;
+}
+
+public String getCom_backend_option() {
+	return com_backend_option;
+}
+
+public void setCom_backend_option(String com_backend_option) {
+	this.com_backend_option = com_backend_option;
+}
+ 
+}
